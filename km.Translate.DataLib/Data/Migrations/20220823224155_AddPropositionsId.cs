b357b141fbb9d4853d1ed09da08e120e34cc5c52
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-08-24 01:03:03 +0100

    chore(data layer): applied migrations

---
+++ km.Translate.DataLib/Data/Migrations/20220823224155_AddPropositionsId.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace km.Translate.DataLib.Data.Migrations
+{
+    public partial class AddPropositionsId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PropositionId",
+                table: "Sentences",
+                newName: "PropositionsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PropositionsId",
+                table: "Sentences",
+                newName: "PropositionId");
+        }
+    }
+}
---
+++ km.Translate.DataLib/Data/Migrations/20220823224641_RemovePropositionsId.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace km.Translate.DataLib.Data.Migrations
+{
+    public partial class RemovePropositionsId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PropositionsId",
+                table: "Sentences");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PropositionsId",
+                table: "Sentences",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
